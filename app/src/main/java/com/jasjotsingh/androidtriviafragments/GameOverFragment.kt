eecf258e59cb240017e5c9763c5c63e20a733c62
Jasjot Singh <jasjot784@gmail.com>
2020-07-15 20:24:19 +0530

    Game over and won listeners added

--- app/src/main/java/com/jasjotsingh/androidtriviafragments/GameOverFragment.kt
+++ app/src/main/java/com/jasjotsingh/androidtriviafragments/GameOverFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.jasjotsingh.androidtriviafragments.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -14,6 +15,11 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game_over, container, false)
+        // Add OnClick Handler for Try Again button
+        binding.tryAgainButton.setOnClickListener{view: View->
+            view.findNavController()
+                .navigate(R.id.action_gameOverFragment_to_gameFragment)}
+
         return binding.root
     }
 }
--- app/src/main/java/com/jasjotsingh/androidtriviafragments/GameWonFragment.kt
+++ app/src/main/java/com/jasjotsingh/androidtriviafragments/GameWonFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.jasjotsingh.androidtriviafragments.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
@@ -14,6 +15,10 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game_won, container, false)
+        // Add OnClick Handler for Next Match button
+        binding.nextMatchButton.setOnClickListener{view: View->
+            view.findNavController()
+                .navigate(R.id.action_gameWonFragment_to_gameFragment)}
         return binding.root
     }
 }
