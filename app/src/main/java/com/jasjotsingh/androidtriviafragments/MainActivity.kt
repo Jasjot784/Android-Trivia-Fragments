d5a857d1fbf69af2d71891cd87cdb82d74101fe9
Jasjot Singh <jasjot784@gmail.com>
2020-07-15 20:31:36 +0530

    Added up button

--- app/src/main/java/com/jasjotsingh/androidtriviafragments/MainActivity.kt
+++ app/src/main/java/com/jasjotsingh/androidtriviafragments/MainActivity.kt
@@ -4,6 +4,8 @@ package com.jasjotsingh.androidtriviafragments
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.jasjotsingh.androidtriviafragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
 
     // TODO (01) Create the new TitleFragment
@@ -22,4 +26,8 @@ class MainActivity : AppCompatActivity() {
     // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
     // In our new TitleFragment
     // R.layout.fragment_title
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
 }
