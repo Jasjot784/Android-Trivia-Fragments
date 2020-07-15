945e975442cc5b045a3fd7c732d2d4d6d1d4d439
Jasjot Singh <jasjot784@gmail.com>
2020-07-15 20:48:17 +0530

    Options menu Added

--- app/src/main/java/com/jasjotsingh/androidtriviafragments/TitleFragment.kt
+++ app/src/main/java/com/jasjotsingh/androidtriviafragments/TitleFragment.kt
@@ -1,12 +1,11 @@
 package com.jasjotsingh.androidtriviafragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.jasjotsingh.androidtriviafragments.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -27,9 +26,19 @@ class TitleFragment : Fragment() {
                 view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 
 }
