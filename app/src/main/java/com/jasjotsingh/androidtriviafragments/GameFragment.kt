4acea8af7bded4301c766782aac61578a911a11c
Jasjot Singh <jasjot784@gmail.com>
2020-07-16 16:33:09 +0530

    Game Won and Game Over fragment navigated using directions

--- app/src/main/java/com/jasjotsingh/androidtriviafragments/GameFragment.kt
+++ app/src/main/java/com/jasjotsingh/androidtriviafragments/GameFragment.kt
@@ -83,14 +83,14 @@ class GameFragment : Fragment() {
                         setQuestion()
                         binding.invalidateAll()
                     } else {
-                        // We've won!  Navigate to the gameWonFragment.
+                        // We've won!  Navigate to the gameWonFragment
                         view.findNavController()
-                            .navigate(R.id.action_gameFragment_to_gameWonFragment)
+                            .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
                     view.findNavController().
-                    navigate(R.id.action_gameFragment_to_gameOverFragment)
+                    navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
 
                 }
             }
