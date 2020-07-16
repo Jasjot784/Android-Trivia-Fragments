f1664f1cff05d8a6e709c2369848986ad90f3688
Jasjot Singh <jasjot784@gmail.com>
2020-07-16 08:15:37 +0530

    About fragment takes to play and system back from there to title fragment

--- app/src/main/java/com/jasjotsingh/androidtriviafragments/AboutFragment.kt
+++ app/src/main/java/com/jasjotsingh/androidtriviafragments/AboutFragment.kt
@@ -5,11 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.jasjotsingh.androidtriviafragments.databinding.FragmentAboutBinding
+import com.jasjotsingh.androidtriviafragments.databinding.FragmentGameWonBinding
 
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val binding: FragmentAboutBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_about, container, false)
+        binding.buttonPlay.setOnClickListener { view: View? ->
+            if (view != null) {
+                view.findNavController().navigate(R.id.action_aboutFragment_to_gameFragment)
+            }
+        }
+        return binding.root
     }
 }
